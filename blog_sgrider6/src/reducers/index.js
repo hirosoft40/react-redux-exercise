10862925e2e4432fda9bd19ab6b7152a3526d817
hirosoft40 <hirosoft40@gmail.com>
2019-04-15 10:42:54 -0500

    update

--- blog_sgrider6/src/reducers/index.js
+++ blog_sgrider6/src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
+import usersReduer from "./usersReducer";
+
 export default combineReducers({
-  posts: postsReducer 
+  users: usersReduer,
+  posts: postsReducer
 });
